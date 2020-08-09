5f2fd70682ef02363be359a73afbe9ef3d004843
crijosicar <12100677+crijosicar@users.noreply.github.com>
2020-08-09 11:53:37 -0500

    convert ot NextJS

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,30 @@
-module.exports = {
-  babelrcRoots: ['.', 'packages/*'],
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = ['next/babel'];
+  const plugins = [
+    ['styled-components'],
+    [
+      'import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'lib',
+        style: 'index.css',
+      },
+    ],
+    [
+      'import',
+      {
+        libraryName: '@ant-design/icons',
+        libraryDirectory: 'lib/icons',
+        camel2DashComponentName: false,
+      },
+      '@ant-design/icons',
+    ],
+  ];
+
+  return {
+    presets,
+    plugins,
+  };
 };
